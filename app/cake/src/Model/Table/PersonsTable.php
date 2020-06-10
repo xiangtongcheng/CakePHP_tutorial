e7564ee4148a7cdb6761bfbb1f0c7051ba001060
MasatoIso <04masato05@gmail.com>
2020-06-10 14:54:20 +0900

    model

---
+++ app/cake/src/Model/Table/PersonsTable.php
@@ -0,0 +1,65 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Persons Model
+ *
+ * @method \App\Model\Entity\Person get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Person newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Person[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Person|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Person saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Person patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Person[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Person findOrCreate($search, callable $callback = null, $options = [])
+ */
+class PersonsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('persons');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('name')
+            ->requirePresence('name', 'create')
+            ->notEmptyString('name');
+
+        $validator
+            ->integer('age')
+            ->allowEmptyString('age');
+
+        $validator
+            ->scalar('mail')
+            ->allowEmptyString('mail');
+
+        return $validator;
+    }
+}
